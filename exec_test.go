defb8e1cd7b8a287b3f1d703df041a1d1c3480d7
haoxin <haoxinst@gmail.com>
2020-09-03 14:53:27 +0800

    use testify

--- exec_test.go
+++ exec_test.go
@@ -4,35 +4,32 @@ import (
 	"strings"
 	"testing"
 
-	. "github.com/pkg4go/assert"
+	"github.com/stretchr/testify/assert"
 )
-
-	a := A{t}
 
 func Test_ExecCommand(t *testing.T) {
 	out, err := ExecCommand("ls", "-a")
 	t.Log(out)
-	a.Equal(err, nil)
-	a.Equal(strings.Contains(out, "exec.go"), true)
-	a.Equal(strings.Contains(out, "exec_test.go"), true)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, strings.Contains(out, "exec.go"), true)
+	assert.Equal(t, strings.Contains(out, "exec_test.go"), true)
 
 	out, err = ExecCommand("foo")
-	a.NotEqual(err, nil)
-	a.NotEqual(out, "")
+	assert.NotEqual(t, err, nil)
+	assert.NotEqual(t,out, "")
 }
 
-	a := A{t}
 func Test_Split(t *testing.T) {
 
-	a.Equal(split("ls -a -l"), []string{"ls", "-a", "-l"})
-	a.Equal(split("ls -a -l "), []string{"ls", "-a", "-l"})
-	a.Equal(split(" ls -a -l"), []string{"ls", "-a", "-l"})
-	a.Equal(split(" ls -a -l "), []string{"ls", "-a", "-l"})
-	a.Equal(split(" ls   -a   -l "), []string{"ls", "-a", "-l"})
+	assert.Equal(t,split("ls -a -l"), []string{"ls", "-a", "-l"})
+	assert.Equal(t, split("ls -a -l "), []string{"ls", "-a", "-l"})
+	assert.Equal(t,split(" ls -a -l"), []string{"ls", "-a", "-l"})
+	assert.Equal(t,split(" ls -a -l "), []string{"ls", "-a", "-l"})
+	assert.Equal(t,split(" ls   -a   -l "), []string{"ls", "-a", "-l"})
 
-	a.Equal(split("cmd \"a b c\""), []string{"cmd", "a b c"})
-	a.Equal(split("cmd -a \"a b c\""), []string{"cmd", "-a", "a b c"})
-	a.Equal(split(" cmd -a \"a b c\" "), []string{"cmd", "-a", "a b c"})
-	a.Equal(split(" cmd -a a/b \"a  b  c\" "), []string{"cmd", "-a", "a/b", "a  b  c"})
-	a.Equal(split(" cmd  -a  a/b  \"a  b  c\" "), []string{"cmd", "-a", "a/b", "a  b  c"})
+	assert.Equal(t,split("cmd \"a b c\""), []string{"cmd", "a b c"})
+	assert.Equal(t,split("cmd -a \"a b c\""), []string{"cmd", "-a", "a b c"})
+	assert.Equal(t,split(" cmd -a \"a b c\" "), []string{"cmd", "-a", "a b c"})
+	assert.Equal(t,split(" cmd -a a/b \"a  b  c\" "), []string{"cmd", "-a", "a/b", "a  b  c"})
+	assert.Equal(t,split(" cmd  -a  a/b  \"a  b  c\" "), []string{"cmd", "-a", "a/b", "a  b  c"})
 }
