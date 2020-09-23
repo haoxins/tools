4e028878652572343593949a2d4eb019c83b4e44
haoxin <haoxinst@gmail.com>
2020-09-23 17:41:13 +0800

    add today

--- time_test.go
+++ time_test.go
@@ -7,3 +7,7 @@ import (
 func Test_Yesterday(t *testing.T) {
 	t.Log("Yesterday is:", Yesterday("Asia/Shanghai", "2006-01-02"))
 }
+
+func Test_Today(t *testing.T) {
+	t.Log("Today is:", Today("Asia/Shanghai", "2006-1-2"))
+}
