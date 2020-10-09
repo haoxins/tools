7f5e56dc36bb96f79853695ef0ced7a6a99c1cec
haoxin <haoxinst@gmail.com>
2020-10-09 11:48:52 +0800

    Add CopyFile

---
+++ fs.go
@@ -0,0 +1,28 @@
+package tools
+
+import (
+	"io"
+	"os"
+)
+
+// CopyFile copy file
+func CopyFile(from, to string) error {
+	src, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(to, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
