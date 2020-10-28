9692b0de26e0cd18f3088878c871df8534128ce6
haoxin <haoxinst@gmail.com>
2020-10-28 21:04:31 +0800

    Deprecated AssertError, add PanicError and FatalError

--- assert.go
+++ assert.go
@@ -6,10 +6,27 @@ import (
 	"github.com/pkg/errors"
 )
 
-// AssertError - Panic if error is not nil
+// AssertError - Deprecated
 func AssertError(err error) {
+	log.Println("Deprecated AssertError, prefer PanicError")
 	if err != nil {
 		log.Println(err)
 		panic(errors.Wrap(err, "[Assert error failed]"))
+	}
+}
+
+// PanicError panic if error not nil
+func PanicError(err error) {
+	if err != nil {
+		log.Println(err)
+		panic(errors.Wrap(err, "[Panic error]"))
+	}
+}
+
+// FatalError fatal if error not nil
+func FatalError(err error) {
+	if err != nil {
+		log.Println(err)
+		log.Fatalln(errors.Wrap(err, "[Fatal error]"))
 	}
 }
