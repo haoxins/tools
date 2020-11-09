98a4754d680d53c9a0b0e602ab5abc5d0b3d73bb
haoxin <haoxinst@gmail.com>
2020-11-09 18:26:07 +0800

    Add deprecated syslog

--- util.go
+++ util.go
@@ -2,10 +2,11 @@ package tools
 
 import (
 	"fmt"
+	"log"
 )
 
 // RightPad right pad
-// Deprecated - github.com/willf/pad
 func RightPad(content string, length string) string {
+	log.Println("Deprecated RightPad, prefer github.com/willf/pad")
 	return fmt.Sprintf("%-"+length+"v", content)
 }
